f472e83b86ea546b4faa1e94598eaf4cb4527b45
amh-42 <anwarmisbaham@gmail.com>
2024-04-23 19:38:27 +0300

    Basic Hotel app configuration

--- dreambackend/urls.py
+++ dreambackend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def hotels(request):
+    return HttpResponse('Hotels List')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user_management/', include('user_management.urls')),
-    path('hotel_management/', include('hotel_management.urls')),
-    path('vr_experience/', include('vr_experience.urls')),
-    # Add more app URLs as needed
+    path('home/', admin.site.urls),
+    path('hotels/', admin.site.urls),
 ]
