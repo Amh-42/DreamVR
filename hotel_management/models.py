e1492d5f178beae973d2f656e17313e2c99b8887
amh-42 <anwarmisbaham@gmail.com>
2024-04-23 19:33:09 +0300

    initial commit

--- hotel_management/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from .user_management.models import HotelAdmin
-
-class HotelPost(models.Model):
-    hotel_admin = models.ForeignKey(HotelAdmin, on_delete=models.CASCADE)
-    post_id = models.CharField(max_length=20, unique=True)
-    # Add other fields as needed
